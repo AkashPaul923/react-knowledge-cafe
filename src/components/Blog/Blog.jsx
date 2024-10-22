b3d30d96c6194918f9c44d9ce069dd7327fa7709
AkashPaul923 <akashpaul923@gmail.com>
2024-10-22 14:14:15 +0600

    add individual blog

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,26 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
-    console.log(blog)
+    const {title, cover,author_img,author,posted_date,reading_time,hashtags} = blog
     return (
         <div>
-            
+            <img src={cover} alt={`pic of ${title}`} />
+            <div className='flex justify-between items-center'>
+                <div className='flex items-center gap-6'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div>
+                        <h2>{author}</h2>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-4xl font-bold">{title}</h2>
+            {
+                hashtags.map((hashtag, idx) => <span key ={idx}><a href=''> #{hashtag} </a></span> )
+            }
         </div>
     );
 };
